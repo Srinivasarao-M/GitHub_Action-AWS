3a6591281c863cf34542beb26762c671c6363c80
Srinivasarao-M <Srinivasaraomaddila16@gmail.com>
2022-12-19 19:08:09 +0530

    EC2 creation

--- ec2.tf
+++ ec2.tf
@@ -1,9 +1,15 @@
+resource "aws_iam_instance_profile" "ssm-profile" {
+  name = "SSM-CONNECT-PROFILE"
+  role = "EC2-SSM-Connect-Role"
+}
+
 resource "aws_instance" "webec2" {
   count = var.web_servers_count #length(var.web_public_subnet_cidr)
 
   ami                    = var.ami
   instance_type          = var.instance_type
   vpc_security_group_ids = [aws_security_group.ec2_public_security_group.id]
+  iam_instance_profile   =  aws_iam_instance_profile.ssm-profile.name
   # subnet_id              = aws_subnet.web_public_subnets[count.index].id
   subnet_id = (count.index % 2) == 0 ? aws_subnet.web_public_subnets[0].id : aws_subnet.web_public_subnets[1].id
   key_name  = aws_key_pair.ec2_keypair.key_name
